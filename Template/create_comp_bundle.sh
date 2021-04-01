524df09fab7990bea27b59c4b132ff26528aa407
Benjamin Bearce <bbearce@gmail.com>
2021-04-01 16:02:10 -0400

    updates

--- Template/create_comp_bundle.sh
+++ Template/create_comp_bundle.sh
@@ -11,6 +11,9 @@ zip -j evaluation_script.zip evaluation_script/*
 
 zip -j Template_Competition_Bundle_2020.zip competition.yaml data.html evaluation.html overview.html terms_and_conditions.html upload_docker_submission.html logo.jpg test_data.zip evaluation_script.zip training_data.zip validation_data.zip
 
+# [3] Create Data submission
+zip -j sample_data_submission.zip sample_submission/*
+
 # [4] Create Docker Submission Zip
 
 zip -j docker_submission_v1.zip \
