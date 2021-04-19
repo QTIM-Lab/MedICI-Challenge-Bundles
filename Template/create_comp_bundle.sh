e809e0131ca3cd52b4ff323957df96c2df241af5
Benjamin Bearce <bbearce@gmail.com>
2021-04-19 15:34:59 -0400

    Template/create_comp_bundle.sh

--- Template/create_comp_bundle.sh
+++ Template/create_comp_bundle.sh
@@ -9,10 +9,23 @@ zip -j evaluation_script.zip evaluation_script/*
 
 # [3] Bundle
 
-zip -j Template_Competition_Bundle_2020.zip competition.yaml data.html evaluation.html overview.html terms_and_conditions.html upload_docker_submission.html logo.jpg test_data.zip evaluation_script.zip training_data.zip validation_data.zip
+zip -j Template_Competition_Bundle_2020.zip \
+       competition.yaml \
+       data.html \
+       evaluation.html \
+       overview.html \
+       terms_and_conditions.html \
+       upload_docker_submission.html \
+       logo.jpg \
+       test_data.zip \
+       evaluation_script.zip \
+       training_data.zip \
+       validation_data.zip
 
 # [3] Create Data submission
 zip -j sample_data_submission.zip sample_submission/*
+or
+zip -j sample_data_submission.zip sample_submission/sample_submission.csv*
 
 # [4] Create Docker Submission Zip
 
